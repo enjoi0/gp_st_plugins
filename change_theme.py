d95ed1536edc9be11fd8fcf574e47a78313b1631
Geordie Powers <geordie.powers@gmail.com>
2017-07-27 16:01:45 -0400

    change_theme: get settings every time plugin is run, rather than once at startup

--- change_theme.py
+++ change_theme.py
@@ -4,13 +4,14 @@ import sublime_plugin
 SETTINGS_PATH = "Preferences.sublime-settings"
 
 class ChangeTheme(sublime_plugin.WindowCommand):
-    settings = sublime.load_settings(SETTINGS_PATH)
-
     def run(self, theme = 'dark'):
+        settings = sublime.load_settings(SETTINGS_PATH)
+
         if theme == 'dark':
-            self.settings.set("theme", "Cyanide - Wood.sublime-theme")
-            self.settings.set("color_scheme", "Packages/gp_st_colorschemes/Dark/nimbostratus_night.tmTheme")
+            settings.set("theme", "Cyanide - Wood.sublime-theme")
+            settings.set("color_scheme", "Packages/gp_st_colorschemes/Dark/nimbostratus_night.tmTheme")
         else:
-            self.settings.set("theme", "Boxy Yesterday.sublime-theme")
-            self.settings.set("color_scheme", "Packages/gp_st_colorschemes/Light/NoNameYet.tmTheme")
+            settings.set("theme", "Boxy Yesterday.sublime-theme")
+            settings.set("color_scheme", "Packages/gp_st_colorschemes/Light/NoNameYet.tmTheme")
+            
         sublime.save_settings(SETTINGS_PATH)
